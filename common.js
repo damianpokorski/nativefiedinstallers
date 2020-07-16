cb4df18334ab57a0c3a39005e893e63e60840ec3
Damian Pokorski <damian@payl8r.com>
2020-07-16 09:48:19 +0100

    Added debian installer builder

--- common.js
+++ common.js
@@ -15,56 +15,86 @@ module.exports = {
     const installPath = `${installRoot}/${appName}`;
     const binaryLinkPath = `${homedir}/.local/bin`;
     
-    // Optional install
-    return (new Confirm('Would you like to install the generated app?')).ask(function(yes) {
-      if(!yes) {
-        console.log(`Skipping install.`);
-        return;
-      }
 
-      // Base install path
-      if (!fs.existsSync(installRoot)) {
-        console.log(`${installRoot} not found, creating.`)
-        fs.mkdirSync(installRoot);
-        
-      }
+    // Base install path
+    if (!fs.existsSync(installRoot)) {
+      console.log(`${installRoot} not found, creating.`)
+      fs.mkdirSync(installRoot);
       
-      // Remove old version if it doesnt exist
-      console.log(`Checking for previous generated installs ${installRoot}..`);
-      if(fs.existsSync(installPath)) {
-        console.log(`Found previous install  (${installRoot}) - removing.`);
-        fs.rmdirSync(installPath, {recursive: true });
-      }
-      
-      // Move to install path
-      console.log(`Moving to ${installPath}`);
-      const generatedFolderPath = `${generatedDir}/${fs.readdirSync(generatedDir).shift()}`;
-      fsx.moveSync(generatedFolderPath, installPath);
+    }
 
-      // Remove empty folder
-      fsx.rmdirSync(generatedDir);
+    const generatedFolderPath = `${generatedDir}/${fs.readdirSync(generatedDir).shift()}`;
 
-      // Find binary file
-      const binaryPath = `${installPath}/${fs.readdirSync(installPath).filter(file => file.startsWith(appName)).shift()}`;
-      console.log(binaryPath);
-
-      return (new Confirm('Would you like to create binary shortcut? (Unix only)')).ask(function(yes) {
-        if (!yes) {
+    return (new Confirm(`Would you like to create installer? Debian only`))
+      .run()
+      .then(function(yes) {
+        if(!yes) {
+          console.log(`Skipping installer building.`);
           return;
-        } 
-        if(!fs.existsSync(binaryLinkPath)) {
-          console.log(`${binaryLinkPath} not found, creating.`)
-          fs.mkdirSync(binaryLinkPath, { recursive: true });
         }
-
-        if(fs.existsSync(`${binaryLinkPath}/${appName}`)) {
-          console.log(`${binaryLinkPath}/${appName} exists, removing.`)
-          fs.unlinkSync(`${binaryLinkPath}/${appName}`);
+        const installer = require('electron-installer-debian');
+        const options = {
+          src: generatedFolderPath,
+          dest: 'dist/installers/',
+          arch: 'amd64',
+          bin: appName,
+          name: appName,
+          productName: appName,
+          genericName: appName
         }
+        return installer(options).then(result => {
+          console.log(`Successfully created package in ${options.dest}`)
+        }).catch(err => {
+          console.error(err, err.stack);
+        });
+      }).then(() => {
+        return (new Confirm(`Would you like to  install the app into your home directory ${installPath}`)).ask(function(yes) {
+          if(!yes) {
+            console.log(`Skipping install.`);
+            return;
+          }
+    
+          // Remove old version if it doesnt exist
+          console.log(`Checking for previous generated installs ${installRoot}..`);
+          if(fs.existsSync(installPath)) {
+            console.log(`Found previous install  (${installRoot}) - removing.`);
+            fs.rmdirSync(installPath, {recursive: true });
+          }
+          
+          // Move to install path
+          console.log(`Moving to ${installPath}`);
+    
+          fsx.moveSync(generatedFolderPath, installPath);
+    
+          // Remove empty folder
+          fsx.rmdirSync(generatedDir);
+    
+          // Find binary file
+          const binaryPath = `${installPath}/${fs.readdirSync(installPath).filter(file => file.startsWith(appName)).shift()}`;
+          console.log(binaryPath);
+    
+          return (new Confirm('Would you like to create binary shortcut? (Unix only)')).ask(function(yes) {
+            if (!yes) {
+              return;
+            } 
+            if(!fs.existsSync(binaryLinkPath)) {
+              console.log(`${binaryLinkPath} not found, creating.`)
+              fs.mkdirSync(binaryLinkPath, { recursive: true });
+            }
+    
+            if(fs.existsSync(`${binaryLinkPath}/${appName}`)) {
+              console.log(`${binaryLinkPath}/${appName} exists, removing.`)
+              fs.unlinkSync(`${binaryLinkPath}/${appName}`);
+            }
+    
+            fs.symlinkSync(binaryPath, `${binaryLinkPath}/${appName}`);
+            console.log(`Shortcut created.`);
+          });
+        });
+      })
 
-        fs.symlinkSync(binaryPath, `${binaryLinkPath}/${appName}`);
-        console.log(`Shortcut created.`);
-      });
-    });
+
+    // Optional install
+    
   }
 }
